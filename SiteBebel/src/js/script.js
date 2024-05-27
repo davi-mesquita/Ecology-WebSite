7ca93c1781985e7b1e8ded5e911496d6cffc032d
Davi Mesquita da Fonseca <77731429+davi-mesquita@users.noreply.github.com>
2024-05-27 11:08:42 -0300

    Delete SiteBebel directory

--- SiteBebel/src/js/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) { // Mostra o header após 50px de rolagem
-      header.classList.add('show');
-    } else {
-      header.classList.remove('show');
-    }
-  });
