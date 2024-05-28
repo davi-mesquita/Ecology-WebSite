4e1bc8d9ab2e42aa93b264c0b3169dae926c6304
Davi Mesquita da Fonseca <davimesquita9@gmail.com>
2024-05-28 16:22:38 -0300

    Add EmailJS to send message

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,13 +1,18 @@
 const contactForm = document.getElementById('contactForm');
 const YOUR_SERVICE_ID = 'service_rwsbweb'; 
-const YOUR_TEMPLATE_ID = 'template_7eoy51d'; 
-const YOUR_PUBLIC_KEY = 'fxXp84IHnRCxebqZ9'; 
+const YOUR_TEMPLATE_ID = 'template_im79ami'; 
 
 contactForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  // Coleta os dados do formulário
+  (function() {
+    emailjs.init({
+      publicKey: "fxXp84IHnRCxebqZ9",
+    });
+  })();
+
   const formData = new FormData(contactForm);
+
   const templateParams = {
     to_name: 'Davi Mesquita',
     to_email: 'davimesquita9@gmail.com',
@@ -16,8 +21,9 @@ contactForm.addEventListener('submit', (event) => {
     message: formData.get('mensagem')
   };
 
-  // Envia o email usando o EmailJS
-  emailjs.send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, templateParams, YOUR_PUBLIC_KEY)
+
+
+  emailjs.send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, templateParams)
     .then((response) => {
       alert('Mensagem enviada com sucesso!');
       contactForm.reset();
